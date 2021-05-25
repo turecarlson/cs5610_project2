c5ced9a1d137f47980a409db41e3a217d54afb27
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2021-05-25 16:01:52 -0400

    defined card props in an enum

---
+++ src/model/CardProperties.js
@@ -0,0 +1,30 @@
+const CARD_SHAPES = {
+    SHAPE_1: 'SHAPE_1',
+    SHAPE_2: 'SHAPE_2',
+    SHAPE_3: 'SHAPE_3'
+};
+
+const CARD_COLORS = {
+    RED:    'RED',
+    BLUE:   'BLUE',
+    GREEN:  'GREEN'
+};
+
+const CARD_NUMBERS = {
+    ONE:    'ONE',
+    TWO:    'TWO',
+    THREE:  'THREE'
+};
+
+const CARD_SHADINGS = {
+    SOLID: 'SOLID',
+    STRIPED: 'STRIPED',
+    OUTLINE: 'OUTLINE'
+};
+
+module.exports = {
+    CARD_SHAPES,
+    CARD_COLORS,
+    CARD_NUMBERS,
+    CARD_SHADINGS
+};
