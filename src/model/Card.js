0e8100a97c509a0973400c50e0159e005bc79eab
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2021-05-25 15:48:13 -0400

    wrote Card.js

---
+++ src/model/Card.js
@@ -0,0 +1,10 @@
+class Card {
+    constructor(shape, color, number, shading) {
+        this.shape      = shape;
+        this.color      = color;
+        this.number     = number;
+        this.shading    = shading;
+    }
+}
+
+module.exports = Card;
